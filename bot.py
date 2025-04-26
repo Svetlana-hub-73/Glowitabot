8387cf2070e0de0d541bffdf1568d7f2fdc8ee43
Svetlana <s9031031640@gmail.com>
2025-04-26 18:17:43 +0200

    Фикс запуска сервера Flask на Render

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
+from flask import Flask, request
 import telebot
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN = "7867244578:AAHYaQ-uG93U9XZ4zkfIjasrr_YJnxE2MPM"
 bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет, я GlowitaBot 💫")
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_messages([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
-bot.polling()
+@app.route("/", methods=['GET'])
+def index():
+    return "Я работаю! 🤖", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
